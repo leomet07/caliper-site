245fbf44383301d65d1fa8aebb4bbefaf744181e
leomet07 <leomet07@gmail.com>
2023-12-29 13:30:05 -0500

    Configure collection sorting by specified index

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,15 @@
 const CleanCSS = require("clean-css");
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("fonts");
 	eleventyConfig.addPassthroughCopy("**/*.jpg");
 
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
 	});
 
-	// Filters, custom functions to use in njk templates
-	eleventyConfig.addFilter("readableDate", (dateObj) => {
-		dateObj ??= new Date(); // if null or undefined, then create new obj with current time
-
-		// Options so date looks like "August 6, 2023"
-		const dateOptions = {
-			year: "numeric",
-			month: "long",
-			day: "numeric",
-		};
-
-		return dateObj.toLocaleDateString("en-us", {
-			timeZone: "utc",
-			...dateOptions,
-		}); // Use utc tz so that no tz conversion occurs
-	});
-
-	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-		dateObj ??= new Date(); // if null or undefined, then create new obj with current time
-
-		// Use utc tz so that no tz conversion occurs
-		// Convert to YYYY/MM/DD, then replace forward slashes with dashes to return YYYY-MM-DD
-		return dateObj
-			.toLocaleDateString("en-ZA", { timezone: "utc" })
-			.replace(/\//g, "-");
+	eleventyConfig.addCollection("winter2023", function (collection) {
+		return collection
+			.getFilteredByTag("winter2023")
+			.sort((a, b) => a.data.index - b.data.index);
 	});
 
 	return {
