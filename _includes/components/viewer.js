e3225a99680ae3a10cf50e0c0e93349b422e38b6
leomet07 <leomet07@gmail.com>
2024-02-04 17:36:19 -0500

    Update bug in viewer and add background color

--- _includes/components/viewer.js
+++ _includes/components/viewer.js
@@ -1,3 +1,7 @@
+let leftviewer = document.getElementById("viewer-left-image");
+let rightviewer = document.getElementById("viewer-right-image");
+let mobileviewer = document.getElementById("viewer-mobile-image");
+
 document
 	.getElementById("desktop-right-arrow")
 	.addEventListener("click", () => desktopModify(2));
@@ -6,12 +10,10 @@ document
 	.addEventListener("click", () => desktopModify(-2));
 document
 	.getElementById("mobile-right-arrow")
-	.addEventListener("click", () => desktopModify(1));
+	.addEventListener("click", () => mobileModify(1));
 document
 	.getElementById("mobile-left-arrow")
-	.addEventListener("click", () => desktopModify(-1));
-let leftviewer = document.getElementById("viewer-left");
-let rightviewer = document.getElementById("viewer-right");
+	.addEventListener("click", () => mobileModify(-1));
 
 function desktopModify(mod) {
 	const leftsrc = leftviewer.getAttribute("src");
@@ -21,10 +23,19 @@ function desktopModify(mod) {
 	rightviewer.setAttribute("src", modifySrc(rightsrc, mod));
 }
 
-function modifySrc(src, add) {
+function mobileModify(mod) {
+	const mobilesrc = mobileviewer.getAttribute("src");
+	mobileviewer.setAttribute("src", modifySrc(mobilesrc, mod));
+}
+
+function getNum(src) {
 	const splitsrc = src.split("/");
 	const filename = String(splitsrc[splitsrc.length - 1]);
-	let num = Number(filename.split("-")[1].split(".")[0]);
+	return Number(filename.split("-")[1].split(".")[0]);
+}
+
+function modifySrc(src, add) {
+	let num = getNum(src);
 	if (num + add <= Number(params.max) && num + add >= Number(params.min)) {
 		num += add;
 	}
