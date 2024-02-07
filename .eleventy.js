10f8c06d8d6f9855d388f8eb86938dc58e58f84c
leomet07 <leomet07@gmail.com>
2024-02-07 00:18:42 -0500

    Add contributors collection

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,26 @@ module.exports = function (eleventyConfig) {
 			.sort((a, b) => a.data.index - b.data.index);
 	});
 
+	eleventyConfig.addCollection("contributors", function (collectionApi) {
+		const contributors = {};
+		collectionApi.getFilteredByTag("winter2023", "spring2023", "winter2024").forEach(element => {
+			console.log(element)
+			const author_slug = element.data.author_slug;
+			if (!(author_slug in contributors)) {
+				contributors[author_slug] = {
+					author_slug: element.data.author_slug,
+					author: element.data.author,
+					pieces: []
+				};
+			}
+			contributors[author_slug].pieces.push({
+				slug: element.fileSlug, // piece slug
+				title: element.data.title
+			}); // push the piece slug into pieces
+		});
+		return contributors;
+	});
+
 	return {
 		dir: {
 			input: "content", // default: "."
